78f69ae905b4cf86bfe44ad0547b1174219deec6
chinmayk26 <korgaonkarchinmay2659@gmail.com>
2022-11-04 19:47:58 +0530

    book page is implemented

---
+++ server/models/book.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const BookSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    imageURL: {
+      type: String,
+      required: true,
+    },
+    bookUrl: {
+      type: String,
+      required: true,
+    },
+    album: {
+      type: String,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("book", BookSchema);
